76abd592fc472a6e6539bc7465846dd57afbd405
IndividualGames90 <IndividualGames@yandex.com>
2023-08-26 09:36:36 -0700

    loading screen start

---
+++ Assets/Scripts/UI/CanvasEventHub.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace IndividualGames.HappyHourStrategyCase
+{
+    /// <summary>
+    /// Exposure of Canvas related events.
+    /// </summary>
+    public class CanvasEventHub : MonoBehaviour
+    {
+        public readonly BasicSignal JoinGame = new();
+
+
+        public void OnJoinGame()
+        {
+            JoinGame.Emit();
+        }
+    }
+}
