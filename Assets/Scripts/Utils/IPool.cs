d66501d0fffe8c3b76c95e29d4caab7b20d6bae6
IndividualGames90 <IndividualGames@yandex.com>
2023-08-27 09:56:51 -0700

    report updated

---
+++ Assets/Scripts/Utils/IPool.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace IndividualGames.HappyHourStrategyCase
+{
+    public interface IPool
+    {
+        public abstract void Initialize();
+        public abstract GameObject Retrieve();
+    }
+}
